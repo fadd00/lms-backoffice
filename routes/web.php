2af9a57364ef16396dc8ab878338261712ffd2c2
fadd00 <fadd00@users.noreply.github.com>
2026-06-01 18:31:47 +0700

    Refactor routes to enhance organization and clarity; add resource routes for books, categories, members, and borrowings

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BorrowingController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+    // Route yang bisa diakses Admin & Pustakawan
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('books', BookController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('members', MemberController::class);
+    Route::resource('borrowings', BorrowingController::class);
+
+    // Route KHUSUS Admin
+    Route::middleware(['role:Admin'])->group(function () {
+        Route::resource('users', UserController::class);
+    });
 });
 
 require __DIR__.'/settings.php';
